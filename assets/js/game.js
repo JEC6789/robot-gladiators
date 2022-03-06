0b6ee2db77493b4f7bd1b8a39c1d06c974093689
Jonathan Cushing <firex42069@gmail.com>
2022-03-06 17:46:17 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,9 +70,16 @@ var fight = function(enemyName) {
 }; // End of fight function
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(enemyNames[i]);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
 
 // Game states
